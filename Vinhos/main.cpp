541bbec7310d193c65ca8bd500bef360581c9474
Tiago Andrade <tagowill@gmail.com>
2017-09-05 00:35:27 +0100

    Adding new comments for gh-pages

--- Vinhos/main.cpp
+++ Vinhos/main.cpp
@@ -2,17 +2,25 @@
 
 #include "Vinhos.h"
 #include <QtWidgets/QApplication>
+/**
+@file main.cpp
+*/
 
-/**!
-* This is the main cpp
-* Using Qt amd start a new window
+/**
+@mainpage
+
+This is a test application.
+
+@author Tiago
+@date 05/09/2017
+@version 1.0
 */
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv); /*!< I dont know*/
-	Vinhos w;
-	w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+	QApplication a(argc, argv); /**< Initialize Application*/
+	Vinhos w; /**< Initalize Welcome Window*/
+	w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint); /**< REmove Frame from window*/
 	w.show();
 	return a.exec();
 }
