6690473c8f24387e2c05abf56490f4205e407a04
Tiago Andrade <tagowill@gmail.com>
2017-09-07 00:22:27 +0100

    Change DB file to English words

--- DB/create_db.sql
+++ DB/create_db.sql
@@ -3,69 +3,69 @@ DROP DATABASE IF EXISTS db_vinhos;
 #SET GLOBAL sql_mode = 'TRADITIONAL';
 SET SESSION sql_mode = 'TRADITIONAL';
 
-CREATE DATABASE IF NOT EXISTS db_vinhos CHARACTER SET utf8 COLLATE utf8_bin;
+CREATE DATABASE IF NOT EXISTS db_wines CHARACTER SET utf8 COLLATE utf8_bin;
 
-USE db_vinhos;
+USE db_wines;
 
-CREATE table Morada
+CREATE table Address
 (
-	ID_Morada int auto_increment NOT NULL primary key,
-    Rua nvarchar(20) NOT NULL,
-    NPorta nvarchar(20) NOT NULL,
-    Andar nvarchar(20),
-    Porta nvarchar(20),
+	ID_Address int auto_increment NOT NULL primary key,
+    Street nvarchar(20) NOT NULL,
+    NDoor nvarchar(20) NOT NULL,
+    Floor nvarchar(20),
+    Door nvarchar(20),
     CP nvarchar(20) NOT NULL,
-    Localidade nvarchar(20) NOT NULL
+    Locality nvarchar(20) NOT NULL
 );
 
-CREATE table Clientes
+CREATE table Clients
 (
 	NIF int NOT NULL PRIMARY KEY,
-    ID_Morada int NOT NULL,
-	nome nvarchar(50) NOT NULL,
-	apelido nvarchar(50) NOT NULL,
-	movel int NOT NULL,
-	email nvarchar(255),
-    FOREIGN KEY (ID_Morada) REFERENCES db_vinhos.Morada(ID_Morada)
+    ID_Address int NOT NULL,
+	Name nvarchar(50) NOT NULL,
+	Surname nvarchar(50) NOT NULL,
+	Mobile int NOT NULL,
+	Email nvarchar(255),
+    FOREIGN KEY (ID_Address) REFERENCES db_wines.Address(ID_Address)
 );
 
 ######TEST#############
 
-INSERT INTO db_vinhos.Morada(Rua, NPorta, CP, Localidade)
+INSERT INTO db_wines.Address(Street, NDoor, CP, Locality)
 VALUES (1,1,1,1);
 
-INSERT INTO db_vinhos.Clientes (NIF, ID_Morada,nome, apelido,movel)
+INSERT INTO db_wines.Clients (NIF, ID_Address, Name, Surname, Mobile)
 VALUES (999999999,1,'Tiago', 'Andrade', 912325454);
 
 
-INSERT INTO db_vinhos.Clientes (NIF, ID_Morada,nome, apelido,movel)
+INSERT INTO db_wines.Clients (NIF, ID_Address, Name, Surname, Mobile)
 VALUES (999999991,1,'José', 'Andrade', 912325454);
 
 ######END TEST#############
 
-CREATE table TipoVinhos
+CREATE table WineTypes
 (
-	ID_tipovinhos int NOT NULL auto_increment PRIMARY KEY,
-	ano int NOT NULL,
-	volume int NOT NULL,
-	tipo nvarchar(35)
+	ID_WineTypes int NOT NULL auto_increment PRIMARY KEY,
+	Year int NOT NULL,
+	Volume int NOT NULL,
+	Type nvarchar(35)
 );
 
-CREATE table Boletim
+CREATE table Report
 (
-	ID_boletim int NOT NULL auto_increment PRIMARY KEY,
+	ID_Report int NOT NULL auto_increment PRIMARY KEY,
 	NIF int NOT NULL,
-	ID_tipovinhos int NOT NULL,
-	data Date,
-	ph float,
-	massavolumetrica float,
-	acidez float,
-	grau float,
-	estrato float,
-	ano int,
-	observacoes nvarchar(700),
-	FOREIGN KEY (NIF) REFERENCES db_vinhos.Clientes(NIF),
-	FOREIGN KEY (ID_tipovinhos) REFERENCES db_vinhos.TipoVinhos(ID_tipovinhos)
+	ID_WineTypes int NOT NULL,
+	Data Date,
+	Ph float,
+	VolumetricMass float,
+	Acidity float,
+	Degree float,
+	Stratum float,
+	Year int,
+	Observations nvarchar(700),
+	FOREIGN KEY (NIF) REFERENCES db_wines.Clients(NIF),
+	FOREIGN KEY (ID_WineTypes) REFERENCES db_wines.WineTypes(ID_WineTypes)
 );
 
 commit;
